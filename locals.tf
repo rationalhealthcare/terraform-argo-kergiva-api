cfd542c8b6bc142d730383f3f0673182fca54cf8
Dylan Turnbull <dylanturn@gmail.com>
2020-10-26 15:05:58 -0500

    updated service port

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   automated_self_heal = true
   automated_prune     = true
   service_name        = "kergiva-api"
-  service_port        = 80
+  service_port        = 5000
   service_protocol    = "TCP"
   service_scheme      = "http"
   labels = merge(var.labels, {
